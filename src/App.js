32e48b8eb8f63941a82eb06940d513e622562b27
Sturgart <wesleyicaro4@gmail.com>
2023-03-05 17:48:56 -0300

    main commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import { v4 as uuidv4 } from 'uuid'
+
+const API = 'http://localhost:5000'
 
 function App() {
+  const [list, setList] = useState([])
+  const [undid, setUndid] = useState([])
+
+  // const handleSubmit = async e => {
+  //   e.preventDefault()
+  //   const circles = {
+  //     id: Math.random,
+  //     circle
+  //   }
+
+  const handleClick = event => {
+    // event.preventDefault()
+
+    const newDot = {
+      id: uuidv4(),
+      clientX: event.clientX,
+      clientY: event.clientY
+    }
+
+    // await fetch(API + '/dots', {
+    //   method: 'POST',
+    //   body: JSON.stringify(newDot),
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // })
+    // // setItems(prevState => [...prevState, newDot])
+    console.log(newDot)
+    setList(prev => [...prev, newDot])
+    setUndid([])
+  }
+
+  const handleUndo = event => {
+    event.stopPropagation()
+    console.log('undo')
+
+    if (list.length === 0) {
+      return
+    }
+
+    const lastItem = list[list.length - 1]
+    setUndid(prev => [...prev, lastItem])
+
+    setList(prev => {
+      const newArr = [...prev].slice(0, -1)
+      return newArr
+    })
+  }
+
+  const handleRedo = event => {
+    event.stopPropagation()
+    console.log('handleRedo')
+
+    if (undid.length === 0) {
+      return
+    }
+
+    const recoveredItem = undid[undid.length - 1]
+    setUndid(prev => {
+      const newArr = [...prev].slice(0, -1)
+      return newArr
+    })
+    setList(prev => [...prev, recoveredItem])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page" onClick={handleClick}>
+      <button onClick={handleUndo}>Desfazer</button>
+      <button onClick={handleRedo}>Refazer</button>
+      {list.map(item => (
+        <span
+          className="dot"
+          style={{ left: item.clientX, top: item.clientY }}
+          key={item.id}
+        />
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
